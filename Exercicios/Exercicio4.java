b111b323ed2f4d15ebb1040cf7c0f2a98f18a457
Jorge lucas <jorgelucas2077@gmail.com>
2024-03-07 05:58:29 -0300

    Impressão de média escolar

---
+++ Exercicios/Exercicio4.java
@@ -0,0 +1,37 @@
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio4 {
+    public static void main(String[]args){
+        Scanner mediaScanner = new Scanner(System.in);
+        
+        int nota1;
+        int nota2;
+        int nota3;
+        int nota4;
+        double media;
+
+        //boolean resultado1 = (valor1 == 3) && (valor2 == 4);
+        System.out.println("Digite a primeira nota:");
+        nota1 = mediaScanner.nextInt();
+
+        System.out.println("Digite a segunda nota");
+        nota2 = mediaScanner.nextInt();
+
+        System.out.println("Digite a terceira nota");
+        nota3 = mediaScanner.nextInt();
+
+        System.out.println("Digite a quarta nota");
+        nota4 = mediaScanner.nextInt();
+
+        media = (nota1 + nota2 + nota3 + nota4) / 4;
+        
+
+        System.out.println("A média é:" + media);
+
+        mediaScanner.close();
+}
+}
+    
+    
